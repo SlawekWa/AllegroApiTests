4e9e4b8a0ac7e5d6bbb4874679144f07996135ab
SlawekWa <slawomir.wawrzyn@gmail.com>
2020-09-26 12:43:46 -0600

    Update authenticate.py
    remove vulnerable data

--- fixtures/authenticate.py
+++ fixtures/authenticate.py
@@ -6,8 +6,8 @@ import pytest
 @pytest.fixture(scope="module", autouse=True)
 def authenticate():
     base_url = 'https://allegro.pl/auth/oauth/token?grant_type=client_credentials'
-    username = '616a5068584f4545bb3180e7fec4bf83'
-    password = 'y05Zj1Eh39cf38ZO3UCUyGmUuTxwsmqVGYwy6zoiOG77NkNarNYiJJDeGUDZ91k1'
+    username = 'provide client id'
+    password = 'provide client secret'
 
     credentials = (username + ':' + password).encode('utf-8')
     base64_encoded_credentials = base64.b64encode(credentials).decode('utf-8')
